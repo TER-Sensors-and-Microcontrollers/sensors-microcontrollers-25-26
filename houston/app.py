ac8e618fadcb91b71cc1dd5271a522169d81655e
magmabandit <alexhenrylee@gmail.com>
2025-09-30 18:45:37 -0400

    Update pip dependencies in Houston, Add flask template files

---
+++ houston/app.py
@@ -0,0 +1,73 @@
+# Importing flask module in the project is mandatory
+# An object of Flask class is our WSGI application.
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+import threading
+from multiprocessing import shared_memory
+import time
+import atexit
+# import serial
+#from ER-sensors-microcontrollers.pi.globals import *
+import numpy as np
+
+# Flask constructor takes the name of 
+# current module (__name__) as argument.
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+# The route() function of the Flask class is a decorator, 
+# which tells the application which URL should call 
+# the associated function.
+@app.route('/')
+# ‘/’ URL is bound with run() function. 
+def display_index():
+    
+    send_to_html = 67
+    # return render_template("index.html", array_data = shm_data)
+    return render_template("index.html", data = send_to_html)
+
+# main driver function
+if __name__ == '__main__':
+
+    # Runs the app using socketio for real-time data updates from the
+    # shared memory.
+    socketio.run(app, host="0.0.0.0", port=5000, debug = True, allow_unsafe_werkzeug=True)
+
+
+
+
+
+
+###############################################################################
+###############################################################################
+###############################################################################
+
+# TODO : use identical logic to instead grab from SQLite database
+
+# Reads the shared memory and creates an array called data that contains the
+# first 4 elements of the shared memory array.
+# def read_shm():
+#     try:
+#         existing_shm = shared_memory.SharedMemory(name="mem123")
+#         arr = np.ndarray(shape=10, dtype=np.float32, buffer=existing_shm.buf)
+#         # Convert array to string for display
+#         data = [arr[i].tolist() for i  in range(3)]
+#         existing_shm.close()
+
+#         return data
+#     except FileNotFoundError:
+#         return ["Error: Read from Shared Memory Failed."]
+
+# # Emits the signal 'update_data' to the index.html file to run the javascript
+# # code in the file.
+# def background_thread():
+#     while True:
+#         shm_data = read_shm()
+#         socketio.emit('update_data', {'array_data': shm_data})
+#         time.sleep(0.5)
+
+# # Creates a background thread that continously updates the display with
+# # updated data from the shared memory.
+# thread = threading.Thread(target = background_thread, daemon = True)
+# thread.start()
+
