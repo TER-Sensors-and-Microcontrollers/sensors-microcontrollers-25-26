2a7d7f77bea08ea0bd439b852414c06e2bdbf38b
magmabandit <alexhenrylee@gmail.com>
2026-04-21 21:17:59 -0400

    make shell script print pi ip address

--- houston/scripts/startup.sh
+++ houston/scripts/startup.sh
@@ -4,6 +4,8 @@ while ! hostname -I > /dev/null 2>&1; do
     echo "Waiting for network..."
     sleep 1
 done
+echo "My wlan0 ip address is: "
+ip addr show wlan0 | awk '/inet / {print $2}' | cut -d/ -f1
 echo "Starting processes..."
 python3 ~/sensors-microcontrollers/houston/app.py &
 python3 ~/sensors-microcontrollers/houston/serial-reader.py & 
